1e73e0b1dedd48fe137fb97cf869241477d5bef2
JoseCarrascoPujante <jose.carrasco.pujante@gmail.com>
2023-05-26 18:56:02 +0200

    Update fig1_4PanelsPlusZooms.m

--- main scripts/pasos3/fig1_4PanelsPlusZooms.m
+++ main scripts/pasos3/fig1_4PanelsPlusZooms.m
@@ -1,7 +1,7 @@
 function figure1 = fig1_4PanelsPlusZooms(coordinates, destination_folder)
 
 field_names = fieldnames(coordinates) ;
-figure('Position', [770 10 500 1500])
+figure('Position', [630 10 650 1500])
 figure1 = tiledlayout(3,1,'TileSpacing','tight','Padding','tight') ;
 
 %% Panels 1-4
@@ -11,24 +11,24 @@ fig1_1.Layout.TileSpan = [2 1];
 panels = {'SinEstimulo','Galvanotaxis','Quimiotaxis','Induccion'};
 indexes = {
 {
-{1 2}
-{1}
-{1 2}
+{1,2,4,6,12,13,14,15,17,18,19,22,24,26,28,29,30,32,34,35}
+{1,3,5,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,24}
+{1,2,4,6,7,9,10,14,16,17,18,20,21,22,23,24,26,27,28,29}
 };
 {
 {1:20}
-{1}
+{1,2,3,4,5,6,8,9,12,15,16,17,18,20,22,23,24,25,26,27}
 {1:20}
 };
 {
-{1:20}
-{1}
-{1}
+{10:30}
+{1,2,3,5,6,7,13,14,16,17,18,19,20,21,22,25,26,27,28,29}
+{1,2,3,4,6,12,14,19,20,21,22,23,26,27,28,29,30,32,33,34}
 };
 {
-{1:20}
-{1}
-{37 38 39 40 41 42 43}
+{20:40}
+{10}
+{10}
 }
 };
 for i = 1:length(panels)
@@ -57,10 +57,18 @@ for i = 1:length(panels)
     end
 %     set(ax,'YTickLabel',[],'YTick',[]);
     ax.FontSize = 7;
-    MaxX = max(abs(ax.XLim));    MaxY = max(abs(ax.YLim)+.75);
-    axis([-MaxX MaxX -MaxY MaxY]);
+    MaxX = max(abs(ax.XLim)+1);    MaxY = max(abs(ax.YLim)+1);
+%     axis([-MaxX MaxX -MaxY MaxY]);
     xline(0,'-','Alpha',1,'Color',[0 0 0]); % xline and yline cannot be sent to back
     yline(0,'-','Alpha',1,'Color',[0 0 0]);
+    axis equal
+    if MaxX > MaxY
+        axis([-MaxX MaxX -MaxY*(MaxX/MaxY) MaxY*(MaxX/MaxY)]);
+    elseif MaxY > MaxX
+        axis([-MaxX*(MaxY/MaxX) MaxX*(MaxY/MaxX) -MaxY MaxY]);
+    elseif MaxY == MaxX
+        axis([-MaxX MaxX -MaxY MaxY]);
+    end
 %     daspect([1 1 1])
 end
 
@@ -119,9 +127,9 @@ plot(coordinates.(scenario2).scaled_x(:,Ameba_w_stim), ...
 
 % Define bounds of the rectangle
 left = -1.35;
-bottom = -2.9;
+bottom = -2.85;
 width = 0.3;
-height = 0.15;
+height = 0.1;
 
 % Display the rectangle
 hold(ax1,'on');
